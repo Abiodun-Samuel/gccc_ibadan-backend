6ca29f55224a0e2a618bd08902b746b262580ccf
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2026-02-24 14:59:47 +0100

    feat: code revamp

--- app/Http/Requests/StoreUsherAttendanceRequest.php
+++ app/Http/Requests/StoreUsherAttendanceRequest.php
@@ -11,7 +11,7 @@ class StoreUsherAttendanceRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return $this->user()->hasAnyRole(['admin', 'leader']);
+        return $this->user()->hasAnyRole(['admin', 'leader', 'member']);
     }
 
     /**
